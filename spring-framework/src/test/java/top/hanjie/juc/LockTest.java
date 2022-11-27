6e0e3edb13337060515cfeeba406d1ac803d5abb
daimao <1432345840@qq.com>
2022-11-27 20:56:12 +0800

    juc 测试类

---
+++ spring-framework/src/test/java/top/hanjie/juc/LockTest.java
@@ -0,0 +1,44 @@
+package top.hanjie.juc;
+
+import lombok.SneakyThrows;
+
+/**
+ * 锁测试
+ *
+ * @author 黄汉杰
+ */
+public class LockTest {
+
+    private static final String res1 = "res_1";
+    private static final String res2 = "res_2";
+
+    @SneakyThrows
+    public static void getRes1ThenRes2() {
+        String threadName = Thread.currentThread().getName();
+        synchronized (res1) {
+            System.out.println(threadName + " get: " + res1);
+            Thread.sleep(3000);
+            synchronized (res2) {
+                System.out.println(threadName + " get: " + res2);
+            }
+        }
+    }
+
+    @SneakyThrows
+    public static void getRes2ThenRes1() {
+        String threadName = Thread.currentThread().getName();
+        synchronized (res2) {
+            System.out.println(threadName + " get: " + res2);
+            Thread.sleep(3000);
+            synchronized (res1) {
+                System.out.println(threadName + " get: " + res1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Thread(LockTest::getRes1ThenRes2).start();
+        new Thread(LockTest::getRes2ThenRes1).start();
+    }
+
+}
