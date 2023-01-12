e57b1f5d6b5acf04a831a5c599b8dfa01210599d
HANJJIE <huanghj@dragonpass.com.cn>
2023-01-12 16:39:34 +0800

    Gc rule and thread state

---
+++ spring-framework/src/test/java/top/hanjie/juc/ThreadStateTest.java
@@ -0,0 +1,14 @@
+package top.hanjie.juc;
+
+/**
+ * 线程状态测试
+ *
+ * @author 黄汉杰
+ */
+public class ThreadStateTest {
+
+    public static void main(String[] args) {
+        // todo 线程状态测试
+    }
+
+}
---
+++ spring-framework/src/test/java/top/hanjie/jvm/gc/Rule.java
@@ -0,0 +1,14 @@
+package top.hanjie.jvm.gc;
+
+/**
+ * GC 规则测试
+ *
+ * @author 黄汉杰
+ */
+public class Rule {
+
+    public static void main(String[] args) {
+
+    }
+
+}
