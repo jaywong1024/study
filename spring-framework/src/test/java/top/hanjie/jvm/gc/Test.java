8588c5e38a8628ccff1311efded2eab496715ae0
daimao <1432345840@qq.com>
2023-02-12 22:46:17 +0800

    虚拟机栈测试类

---
+++ spring-framework/src/test/java/top/hanjie/jvm/gc/Test.java
@@ -0,0 +1,31 @@
+package top.hanjie.jvm.gc;
+
+/**
+ * 虚拟机栈测试类
+ *
+ * @author 黄汉杰
+ * <p>描述：虚拟机栈测试类<p>
+ * <p>创建时间：2023/1/6 20:33<p>
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        int res = a();
+        System.out.println(res);
+    }
+
+    public static int a() {
+        return b();
+    }
+
+    public static int b() {
+        return c();
+    }
+
+    public static int c() {
+        int a = 10;
+        int b = 20;
+        return a + b;
+    }
+
+}
