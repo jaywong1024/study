9c7753894b0f2832dbe80db8711f948ca6750e41
HANJJIE <huanghj@dragonpass.com.cn>
2023-03-03 10:55:14 +0800

    lock level test and see obj head

--- spring-framework/src/test/java/top/hanjie/concurrent/lock/ObjectHeadInfo.java
+++ spring-framework/src/test/java/top/hanjie/concurrent/lock/ObjectHeadInfo.java
@@ -5,6 +5,22 @@ import lombok.SneakyThrows;
 import org.openjdk.jol.info.ClassLayout;
 
 public class ObjectHeadInfo {
+    @AllArgsConstructor
+    static class SyncRunnable implements Runnable {
+        private final Object obj;
+        @Override
+        @SneakyThrows
+        public void run() {
+            synchronized (obj) {
+                Thread.sleep(10_000);
+            }
+        }
+    }
+    public static int rank(int i, int num, Object obj) {
+        synchronized (obj) {
+            return i < num ? rank(++i, num, obj) : i;
+        }
+    }
     @SneakyThrows
     public static void main(String[] args) {
         // 无锁
@@ -21,20 +37,14 @@ public class ObjectHeadInfo {
         Object obj3 = new Object();
         new Thread(new SyncRunnable(obj3)).start();
         new Thread(new SyncRunnable(obj3)).start();
-        synchronized (obj3) {
-            System.out.println("========== 重量级锁 ==========");
-            System.out.println(ClassLayout.parseInstance(obj1).toPrintable());
-        }
-    }
-}
-@AllArgsConstructor
-class SyncRunnable implements Runnable {
-    private final Object obj;
-    @Override
-    @SneakyThrows
-    public void run() {
-        synchronized (obj) {
-            Thread.sleep(10_000);
-        }
+        new Thread(new SyncRunnable(obj3)).start();
+        new Thread(new SyncRunnable(obj3)).start();
+        System.out.println("========== 重量级锁 ==========");
+        System.out.println(ClassLayout.parseInstance(obj3).toPrintable());
+        // 偏向锁
+        Object obj4 = new Object();
+        rank(0, 999, obj4);
+        System.out.println("========== 偏向锁 ==========");
+        System.out.println(ClassLayout.parseInstance(obj4).toPrintable());
     }
 }
