d14d0171824fa8c15d273017a8c34f9245cc8bc7
daimao <1432345840@qq.com>
2023-03-19 19:25:02 +0800

    proxy mode

---
+++ spring-framework/src/test/java/top/hanjie/design/structure/proxy/GoodsServiceInterceptor.java
@@ -0,0 +1,30 @@
+package top.hanjie.design.structure.proxy;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+import java.lang.reflect.Method;
+
+class GoodsService {
+    public void save() {
+        System.out.println("保存商品信息");
+    }
+}
+public class GoodsServiceInterceptor implements MethodInterceptor {
+    @Override
+    public Object intercept(Object obj, Method method,
+                            Object[] args,
+                            MethodProxy proxy) throws Throwable {
+        System.out.println("调用方法前");
+        Object result = proxy.invokeSuper(obj, args);
+        System.out.println("调用方法后");
+        return result;
+    }
+    public static void main(String[] args) {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(GoodsService.class);
+        enhancer.setCallback(new GoodsServiceInterceptor());
+        GoodsService goodsService = (GoodsService) enhancer.create();
+        goodsService.save();
+    }
+}
