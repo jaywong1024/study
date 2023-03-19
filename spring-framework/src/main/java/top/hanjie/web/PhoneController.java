f1a35ebdd8e0f1a6d08120ae6172ee4861b8dfa0
daimao <1432345840@qq.com>
2023-03-20 00:17:35 +0800

    valid

--- spring-framework/src/main/java/top/hanjie/web/PhoneController.java
+++ spring-framework/src/main/java/top/hanjie/web/PhoneController.java
@@ -6,12 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.hanjie.dto.Phone;
 import top.hanjie.service.DefaultPhoneService;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("phone")
 public class PhoneController {
     @PostMapping("sayInfo")
-    public void sayInfo(@RequestBody Phone phone) {
+    public void sayInfo(@RequestBody @Valid Phone phone) {
         DefaultPhoneService.getService(phone.getBrand()).sayInfo(phone);
     }
 }
