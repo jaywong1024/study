257cf8a92c464dece920824717d8375e50753900
daimao <1432345840@qq.com>
2023-03-27 20:07:37 +0800

    注释

--- mybatis/src/main/java/top/hanjie/dto/UserDto.java
+++ mybatis/src/main/java/top/hanjie/dto/UserDto.java
@@ -2,7 +2,6 @@ package top.hanjie.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
-
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
@@ -11,16 +10,31 @@ public class UserDto {
     public static class Common {
         @Data
         public static class Out {
+            /**
+             * 主键
+             */
             private Long id;
+            /**
+             * 用户名
+             */
             private String username;
+            /**
+             * 生日
+             */
             @JsonFormat(pattern = "yyyy-MM-dd")
             private Date birthday;
+            /**
+             * 头像 url
+             */
             private String avatarUrl;
         }
     }
     public static class GetOne {
         @Data
         public static class In {
+            /**
+             * 主键
+             */
             @NotNull(message = "dto.user.id.required")
             private Long id;
         }
@@ -28,11 +42,23 @@ public class UserDto {
     public static class Save {
         @Data
         public static class In {
+            /**
+             * 主键
+             */
             private Long id;
+            /**
+             * 用户名
+             */
             @NotBlank(message = "dto.user.username.required")
             private String username;
+            /**
+             * 生日
+             */
             @JsonFormat(pattern = "yyyy-MM-dd")
             private Date birthday;
+            /**
+             * 头像 url
+             */
             private String avatarUrl;
         }
     }
--- mybatis/src/main/java/top/hanjie/entity/User.java
+++ mybatis/src/main/java/top/hanjie/entity/User.java
@@ -6,8 +6,20 @@ import java.util.Date;
 
 @Data
 public class User {
+    /**
+     * 主键
+     */
     private Long id;
+    /**
+     * 用户名
+     */
     private String username;
+    /**
+     * 生日
+     */
     private Date birthday;
+    /**
+     * 头像 url
+     */
     private String avatarUrl;
 }
